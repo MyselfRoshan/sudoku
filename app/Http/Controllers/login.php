316c27bf4a885752769b6ec9229b553a854e71c8
MyselfRoshan <roshanghimire699@protonmail.com>
2023-07-28 23:52:07 +0545

    feat: added user authentication and validation

--- app/Http/Controllers/login.php
+++ app/Http/Controllers/login.php
@@ -1,7 +1,5 @@
 <?php
 
 require_view('login.view.php', [
-    'alerts' => [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]
-
-    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
+    'alerts' => []
 ]);
